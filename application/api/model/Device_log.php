fe87933355c066f7cb68abf2dcd0cf7c4bb1d0e8
liuyuezheng <34496961+liuyuezheng@users.noreply.github.com>
2018-08-23 18:10:25 +0800

    小后台8.23-2

---
+++ application/api/model/Device_log.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/23
+ * Time: 16:53
+ */
+
+namespace app\api\model;
+
+
+use think\Model;
+
+class Device_log extends Model
+{
+
+
+}

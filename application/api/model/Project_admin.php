4dd9e9462d983a6ef50c8341648d6375eba9531e
liuyuezheng <34496961+liuyuezheng@users.noreply.github.com>
2018-08-31 13:09:28 +0800

    8.31 小后台修改密码

--- application/api/model/Project_admin.php
+++ application/api/model/Project_admin.php
@@ -13,6 +13,9 @@ use think\Model;
 
 class Project_admin extends Model
 {
+    public function setPasswordAttr($password){
+        return md5($password);
+    }
     public function device()
     {
         return $this->belongsToMany('Device','project_admin_device','device_id','project_admin_id');
